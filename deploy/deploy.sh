38876032558716b8939f714d5f29e91281f7569a
Ganga Sah <ganga.sah@gmail.com>
2023-04-10 21:38:42 -0700

    Added k8s-secret-traffic-control-license-key.yaml and renamed+modified k8s-serviceaccount-rbac-secretstore.yaml

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,4 @@
 # 1. Replace traffic-control by NAMESPACE in dapr-appconfig-zipkin.yaml's endpointAddress: "http://zipkin.traffic-control.svc.cluster.local:9411/api/v2/spans"
-# 2. Run license-key.sh with NAMESPACE
 
 set -x
 #N="-n traffic-control"
--- deploy/license-key.sh
+++ deploy/license-key.sh
@@ -1,5 +1,5 @@
 set -x
 #N="-n traffic-control"
 #kubectl create secret generic license-key $N --from-file ./license-key
-kubectl create secret generic license-key $N --from-literal=license-key=HX783-5PN1G-CRJ4A-K2L7V
+#kubectl create secret generic license-key $N --from-literal=license-key=HX783-5PN1G-CRJ4A-K2L7V
 kubectl get secret $N -o jsonpath="{.items[].data.license-key}" | base64 -d
