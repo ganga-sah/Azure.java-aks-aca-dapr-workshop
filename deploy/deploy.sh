943e2a541d976e0c37e0c268847d0433b0768f6d
Ganga Sah <ganga.sah@gmail.com>
2023-04-12 20:38:41 -0700

    Moved deploy/*.yaml to deploy/base/ and added dev-traffic-control/kustomization.yaml

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,5 @@
 # 1. Replace traffic-control by NAMESPACE in dapr-appconfig-zipkin.yaml's endpointAddress: "http://zipkin.traffic-control.svc.cluster.local:9411/api/v2/spans"
 
 set -x
-#N="-n traffic-control"
-kubectl apply $N -k . 
+#kubectl apply $N -k ./base 
+kubectl apply -k ./dev-traffic-control 
